6af588ee99ff51a71c9ee83d65fdacfa1228d6b3
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-12-18 05:37:51 +0900

    Fix type problem of i18n

--- src/i18n/langs.ts
+++ src/i18n/langs.ts
@@ -1,7 +1,11 @@
 import * as en from './en.json';
 import * as zh_cn from './zh-cn.json';
 
-export const LANGUAGES = {
+export type Language = Partial<typeof en>;
+
+const _LANGUAGES = {
   en,
   zh_cn
 };
+
+export const LANGUAGES: Record<keyof typeof _LANGUAGES, Language> = _LANGUAGES;
