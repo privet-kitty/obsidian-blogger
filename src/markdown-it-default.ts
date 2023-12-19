a218efe6d658d3de22183e0502b2aa6514960d36
Hugo Sansaqua <privet.kitty99@gmail.com>
2023-12-20 01:35:23 +0900

    Forgot to add src/markdown-it-default

---
+++ src/markdown-it-default.ts
@@ -0,0 +1,46 @@
+import MarkdownIt from 'markdown-it';
+import { MarkdownItImagePluginInstance } from './markdown-it-image-plugin';
+import { isEmpty, trim } from 'lodash-es';
+import { BloggerPluginSettings } from './plugin-settings';
+import MarkdownItMathJax3Plugin from './markdown-it-mathjax3-plugin';
+
+export const getDefaultMarkdownParser = () => {
+  const markdownParser = new MarkdownIt().use(MarkdownItImagePluginInstance.plugin);
+  markdownParser.renderer.rules.image = (tokens, idx) => {
+    const token = tokens[idx];
+    const srcIndex = token.attrIndex('src');
+    const src = token.attrs ? token.attrs[srcIndex][1] : '';
+    const altText = token.content;
+
+    const [alt, size] = altText.split('|');
+    let width;
+    let height;
+    if (!isEmpty(size)) {
+      const sepIndex = size.indexOf('x'); // width x height
+      if (sepIndex > 0) {
+        width = trim(size.substring(0, sepIndex));
+        height = trim(size.substring(sepIndex + 1));
+      } else {
+        width = trim(size);
+      }
+    }
+    if (width) {
+      if (height) {
+        return `<img src="${src}" width="${width}" height="${height}" alt="${alt}">`;
+      }
+      return `<img src="${src}" width="${width}" alt="${alt}">`;
+    } else {
+      return `<img src="${src}" alt="${alt}">`;
+    }
+  };
+  return markdownParser;
+};
+
+export const setupMarkdownParser = (
+  markdownParser: MarkdownIt,
+  settings: BloggerPluginSettings,
+): void => {
+  markdownParser.use(MarkdownItMathJax3Plugin, {
+    outputType: settings.mathJaxOutputType,
+  });
+};
