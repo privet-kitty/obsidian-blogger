e3c385dbe2a8912262137e6f2c342e2e67e937d2
Hugo Sansaqua <privet.kitty99@gmail.com>
2024-02-17 11:20:33 +0900

    Fix version-bump

--- version-bump.mjs
+++ version-bump.mjs
@@ -2,11 +2,9 @@ import { readFileSync, writeFileSync } from 'fs';
 
 const targetVersion = process.env.npm_package_version;
 
-// read minAppVersion from manifest.json and bump version to target version
+// read minAppVersion from manifest.json
 let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '  '));
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync('versions.json', 'utf8'));
